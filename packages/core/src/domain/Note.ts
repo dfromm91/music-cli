2f0d7dc66fa97a5862f4d8e0f17bfeb4a9cbaf35
dfromm91 <dfromm@wgu.edu>
2026-05-13 19:54:25 -0500

    updated models

--- packages/core/src/domain/Note.ts
+++ packages/core/src/domain/Note.ts
@@ -14,7 +14,11 @@ export enum Accidental {
 	Flat = "b",
 }
 
+export type Duration = "w" | "h" | "q" | "e" | "s";
+
 export class Note {
+	public readonly type = "Note";
+
 	constructor(
 		public readonly pitch: Pitch,
 		public readonly accidental: Accidental = Accidental.Natural,
@@ -25,3 +29,32 @@ export class Note {
 		return `${this.pitch}${this.accidental}${this.octave}`;
 	}
 }
+
+export type NoteEvent = {
+	type: "NoteEvent";
+	notes: Note[];
+	duration: Duration;
+};
+
+export type RestEvent = {
+	type: "RestEvent";
+	duration: Duration;
+};
+
+export type ScoreEvent = NoteEvent | RestEvent;
+
+export type Sequence = ScoreEvent[];
+
+export const noteEvent = (
+	notes: Note[],
+	duration: Duration = "q",
+): NoteEvent => ({
+	type: "NoteEvent",
+	notes,
+	duration,
+});
+
+export const restEvent = (duration: Duration = "q"): RestEvent => ({
+	type: "RestEvent",
+	duration,
+});
